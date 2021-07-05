90821f99537a8a7a51d058352d031b7be54e5b48
최진실 <js.choi@theenm.com>
2021-07-05 17:39:02 +0900

    spring

--- hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -2,11 +2,14 @@ package hello.hellospring.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -29,5 +32,12 @@ public class MemberController {
 		member.setName(form.getName());
 		memberService.join(member);
 		return "redirect:/";
+	}
+
+	@GetMapping("/members")
+	public String list(Model model){
+		List<Member> members = memberService.findMembers();
+		model.addAttribute("members",members);
+		return "members/memberList";
 	}
 }
